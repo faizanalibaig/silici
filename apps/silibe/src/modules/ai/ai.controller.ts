2269466b6f788d1064e5dbe69b7353e5a123aeb0
faizanalibaig <riazfiaz6181@gmail.com>
2025-11-19 18:11:37 +0500

    ui changes and made it functional

--- apps/silibe/src/modules/ai/ai.controller.ts
+++ apps/silibe/src/modules/ai/ai.controller.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from "express";
 const { ai } = require("../../config");
 
-exports.generateTweets = async (req: Request, res: Response) => {
+exports.generateTweets = async (req: Request, res: Response): Promise<void> => {
   try {
+    const prompt = req.query.prompt;
+
     const headers = {
       "Content-Type": "text/event-stream",
       Connection: "keep-alive",
@@ -10,18 +12,23 @@ exports.generateTweets = async (req: Request, res: Response) => {
     };
     res.writeHead(200, headers);
 
-    const response = await ai.models.generateContentStream({
-      model: "gemini-2.5-flash",
-      contents: "Explain how AI works",
+    const response: any = await ai.models.generateContentStream({
+      model: "gemini-2.0-flash-lite",
+      contents: `Generate tweet on: ${prompt}`,
+      config: {
+        systemInstruction:
+          "You are Silici, the sister AI of Grok. Your task is to generate multiple tweets based on the given prompt — no explanations, your opinion or direct replies to the prompt. Produce tweets only. Use a natural, human-like tone with “I” and “me.” Keep tweets short, concise, engaging, and informative. Avoid hashtags, excessive emojis, asterisk, and any content involving politics, religion, sex, violence, or other sensitive topics (simply say, sorry I am not allowed to generate tweets these topics). Output tweets as a list, and add “?end?” after each tweet. Keep it clean text.",
+      },
     });
 
-    res.write(`data: Connected to server\n\n`);
     for await (const chunk of response) {
-      console.log(chunk.text);
+      logger.info(chunk.text);
 
       res.write(`data: ${chunk.text}\n\n`);
     }
 
+    res.write("done \n\n");
+    res.write("retry: 0\n\n"); // disable auto reconnect
     res.end();
   } catch (error: any) {
     res.status(500).json({
--- apps/silife/app/page.tsx
+++ apps/silife/app/page.tsx
@@ -1,6 +1,10 @@
 "use client";
 
+import Error from "next/error";
 import { useState } from "react";
+
+import { FaArrowUp } from "react-icons/fa";
+import { FaCopy } from "react-icons/fa";
 
 const BaseUrl = process.env.BaseUrl || "http://localhost:8080";
 
@@ -23,52 +27,63 @@ export default function Page() {
         } else {
           setResponse((prev) => prev + event.data);
         }
-      } catch (error) {
-        console.error("Invalid SSE message:", event.data);
+      } catch (error: unknown) {
+        if (error instanceof Error) {
+          console.error("Invalid SSE message:", event.data);
+        } else {
+          console.error("Unexpected error:", error);
+        }
       }
     };
   };
 
-  /* Split tweets by asterisk */
-  const tweets = response.split("*").filter((t) => t.trim().length > 0);
+  /* Split tweets by ?end? */
+  const tweets = response.split("?end?").filter((t) => t.trim().length > 0);
 
   return (
     <div className="w-screen h-auto flex flex-col items-center pt-10 bg-[#F5F5F5] font-primary">
-      <div className="h-[750px]">
+      <div className="w-full h-[750px] flex flex-col items-center">
         {/* Input Box */}
-        <div className="w-[800px] h-40 flex flex-col bg-white rounded-xl shadow-lg border border-gray-200">
-          <form onSubmit={SubmitChat} className="w-full h-full p-5">
-            <textarea
+        <div className="w-[650px] h-16 flex bg-[#AAC4F5] rounded-xl shadow-lg border border-gray-200">
+          <form
+            onSubmit={SubmitChat}
+            className="w-full h-full flex justify-between items-center p-3"
+          >
+            <input
+              type="text"
               onChange={(e) => setInput(e.target.value)}
               placeholder="Enter a topic to generate tweets..."
-              className="text-black w-full h-3/5 outline-none resize-none bg-transparent"
+              className="text-[#05339C] w-full h-full px-2 outline-none bg-transparent"
             />
 
-            <div className="w-full h-[40%] flex justify-end items-center px-2">
-              <button
-                type="submit"
-                className="bg-[#1D9BF0] text-white px-5 py-2 text-sm rounded-md cursor-pointer shadow-md hover:bg-[#0d8add] transition"
-              >
-                Generate Tweets
-              </button>
-            </div>
+            {/* Submit Button */}
+            <button
+              type="submit"
+              className="bg-[#05339C] text-white px-4 py-3 text-sm rounded-md cursor-pointer shadow-md transition"
+            >
+              <FaArrowUp size={12} />
+            </button>
           </form>
         </div>
 
         {/* Tweets Container */}
-        <div className="w-[800px] mt-10 space-y-4">
-          {tweets.map((tweet, index) => (
-            <div
-              key={index}
-              className="bg-white border border-gray-200 rounded-xl p-4 shadow-sm hover:shadow-md transition flex justify-between items-start"
-            >
-              <p className="text-black whitespace-pre-wrap w-[88%]">{tweet}</p>
+        <div className="w-[650px] mt-12 space-y-4">
+          {["hello this is test tweet", "hello this is test tweet"].map(
+            (tweet, index) => (
+              <div
+                key={index}
+                className="bg-white border border-gray-200 rounded-xl p-3 shadow-sm hover:shadow-md transition flex justify-between items-start"
+              >
+                <p className="text-black whitespace-pre-wrap w-[88%] px-2 flex items-center">
+                  {tweet}
+                </p>
 
-              <button className="ml-3 text-sm bg-gray-200 px-3 py-1 cursor-pointer rounded-md hover:bg-gray-300 transition">
-                Copy
-              </button>
-            </div>
-          ))}
+                <button className="bg-[#05339C] ml-3 text-sm text-white px-3 py-2 cursor-pointer rounded-md transition">
+                  <FaCopy />
+                </button>
+              </div>
+            ),
+          )}
         </div>
       </div>
     </div>
